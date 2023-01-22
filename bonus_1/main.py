b957bc72542c2470591a3ae863fbb65fbc29d378
Kyle Galway <galwayk@sheridancollege.ca>
2023-01-22 16:42:31 -0500

    Complete text transfer to .pdf for bonus.

--- bonus_1/main.py
+++ bonus_1/main.py
@@ -2,6 +2,7 @@ import os
 import glob
 import fpdf
 import pandas
+
 pdf = fpdf.FPDF(orientation="P", format="A4", unit="mm")
 text_files = glob.glob("files/*.txt")
 for text_file in text_files:
@@ -10,6 +11,15 @@ for text_file in text_files:
     pdf.add_page()
     pdf.set_font(family="Times", style='B', size=14)
     pdf.cell(w=0, h=12, txt=text_name, ln=1)
+
+    pdf.set_font(family='Times', size=12)
+    with open(text_file) as file:
+        for line in file:
+            n = 110
+            lines = [line[i: i + n] for i in range(0, len(line), n)]
+            print(lines)
+            for text_line in lines:
+                pdf.cell(w=0, h=10, txt=f"{text_line}", ln=1)
 
 pdf.output("output.pdf")
 
